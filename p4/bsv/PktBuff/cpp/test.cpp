0dfad5ac7d31350c123ec3356bda30fcb284a71b
Han Wang <hwang@cs.cornell.edu>
2015-11-12 00:19:31 -0500

    update pktbuff

--- p4/bsv/PktBuff/cpp/test.cpp
+++ p4/bsv/PktBuff/cpp/test.cpp
@@ -39,6 +39,7 @@
 #include "TbIndication.h"
 #include "TbRequest.h"
 #include "GeneratedTypes.h"
+#include "utils.h"
 
 #ifndef le32
 #define le32    int32_t
@@ -58,28 +59,28 @@
 
 #define DATA_WIDTH 128
 
-//struct pcap_file_header {
-//    u32 magic;
-//    u16 version_major;
-//    u16 version_minor;
-//    s32 thiszone; /* gmt to local correction */
-//    u32 sigfigs;  /* accuracy of timL1 cache bytes userspaceestamps */
-//    u32 snaplen;  /* max length saved portion of each pkt */
-//    u32 linktype; /* data link type (LINKTYPE_*) */
-//} __attribute__((packed));
-//
-//struct pcap_pkthdr_ts {
-//    le32 hts_sec;
-//    le32 hts_usec;
-//}  __attribute__((packed));
-//
-//struct pcap_pkthdr {
-//    struct  pcap_pkthdr_ts ts;  /* time stamp */
-//    le32 caplen;              /* length of portion present */
-//    le32 length;                  /* length this packet (off wire) */
-//}  __attribute__((packed));
-//
-//void mem_copy(const void *buff, int length);
+struct pcap_file_header {
+    u32 magic;
+    u16 version_major;
+    u16 version_minor;
+    s32 thiszone; /* gmt to local correction */
+    u32 sigfigs;  /* accuracy of timL1 cache bytes userspaceestamps */
+    u32 snaplen;  /* max length saved portion of each pkt */
+    u32 linktype; /* data link type (LINKTYPE_*) */
+} __attribute__((packed));
+
+struct pcap_pkthdr_ts {
+    le32 hts_sec;
+    le32 hts_usec;
+}  __attribute__((packed));
+
+struct pcap_pkthdr {
+    struct  pcap_pkthdr_ts ts;  /* time stamp */
+    le32 caplen;              /* length of portion present */
+    le32 length;                  /* length this packet (off wire) */
+}  __attribute__((packed));
+
+void mem_copy(const void *buff, int length);
 
 static TbRequestProxy *device = 0;
 
@@ -138,91 +139,91 @@ public:
 //    MMUIndication(unsigned int id) : MMUIndicationWrapper(id) {}
 //};
 
-//void mem_copy(const void *buff, int packet_size) {
-//
-//    int i, sop, eop;
-//    uint64_t data[2];
-//    int numBeats;
-//
-//    numBeats = packet_size / 8; // 16 bytes per beat for 128-bit datawidth;
-//    if (packet_size % 8) numBeats++;
-//    PRINT_INFO("nBeats=%d, packetSize=%d\n", numBeats, packet_size);
-//    for (i=0; i<numBeats; i++) {
-//        data[i%2] = *(static_cast<const uint64_t *>(buff) + i);
-//        sop = (i/2 == 0);
-//        eop = (i/2 == (numBeats-1)/2);
-//        if (i%2) {
-//            device->writePacketData(data, sop, eop);
-//            PRINT_INFO("%016lx %016lx %d %d\n", data[1], data[0], sop, eop);
-//        }
-//
-//        // last beat, padding with zero
-//        if ((numBeats%2!=0) && (i==numBeats-1)) {
-//            sop = (i/2 == 0) ? 1 : 0;
-//            eop = 1;
-//            data[1] = 0;
-//            device->writePacketData(data, sop, eop);
-//            PRINT_INFO("%016lx %016lx %d %d\n", data[1], data[0], sop, eop);
-//        }
-//    }
-//}
-//
-///**
-// * Send packet on quick_tx device
-// * @param qtx       pointer to a quick_tx structure
-// * @param buffer   full packet data starting at the ETH frame
-// * @param length  length of packet (must be over 0)
-// * @return           length of packet if it was successfully queued, QTX_E_EXIT if a critical error occurred
-// *                  and close needs to be called
-// */
-//static inline int quick_tx_send_packet(const void* buffer, int length) {
-//    assert(buffer);
-//    assert(length > 0);
-//
-//#ifdef EXTRA_DEBUG
-//    printf("[quick_tx] Copying data from %p buffer, length = %d\n",
-//                (buffer, length);
-//#endif
-//    mem_copy(buffer, length);
-//
-//    return length;
-//}
-//
-//bool read_pcap_file(char* filename, void** buffer, long *length) {
-//    FILE *infile;
-//    long length_read;
-//
-//    infile = fopen(filename, "r");
-//    if(infile == NULL) {
-//            printf("File does not exist!\n");
-//            return false;
-//        }
-//
-//    fseek(infile, 0L, SEEK_END);
-//    *length = ftell(infile);
-//    fseek(infile, 0L, SEEK_SET);
-//    *buffer = (char*)calloc(*length, sizeof(char));
-//
-//    /* memory error */
-//    if(*buffer == NULL) {
-//            printf("Could not allocate %ld bytes of memory!\n", *length);
-//            return false;
-//        }
-//
-//    length_read = fread(*buffer, sizeof(char), *length, infile);
-//    *length = length_read;
-//    fclose(infile);
-//
-//    return true;
-//}
-//
+void mem_copy(const void *buff, int packet_size) {
+
+    int i, sop, eop;
+    uint64_t data[2];
+    int numBeats;
+
+    numBeats = packet_size / 8; // 16 bytes per beat for 128-bit datawidth;
+    if (packet_size % 8) numBeats++;
+    PRINT_INFO("nBeats=%d, packetSize=%d\n", numBeats, packet_size);
+    for (i=0; i<numBeats; i++) {
+        data[i%2] = *(static_cast<const uint64_t *>(buff) + i);
+        sop = (i/2 == 0);
+        eop = (i/2 == (numBeats-1)/2);
+        if (i%2) {
+            //device->writePacketData(data, sop, eop);
+            PRINT_INFO("%016lx %016lx %d %d\n", data[1], data[0], sop, eop);
+        }
+
+        // last beat, padding with zero
+        if ((numBeats%2!=0) && (i==numBeats-1)) {
+            sop = (i/2 == 0) ? 1 : 0;
+            eop = 1;
+            data[1] = 0;
+            //device->writePacketData(data, sop, eop);
+            PRINT_INFO("%016lx %016lx %d %d\n", data[1], data[0], sop, eop);
+        }
+    }
+}
+
+/**
+ * Send packet on quick_tx device
+ * @param qtx       pointer to a quick_tx structure
+ * @param buffer   full packet data starting at the ETH frame
+ * @param length  length of packet (must be over 0)
+ * @return           length of packet if it was successfully queued, QTX_E_EXIT if a critical error occurred
+ *                  and close needs to be called
+ */
+static inline int quick_tx_send_packet(const void* buffer, int length) {
+    assert(buffer);
+    assert(length > 0);
+
+#ifdef EXTRA_DEBUG
+    printf("[quick_tx] Copying data from %p buffer, length = %d\n",
+                (buffer, length);
+#endif
+    mem_copy(buffer, length);
+
+    return length;
+}
+
+bool read_pcap_file(char* filename, void** buffer, long *length) {
+    FILE *infile;
+    long length_read;
+
+    infile = fopen(filename, "r");
+    if(infile == NULL) {
+            printf("File does not exist!\n");
+            return false;
+        }
+
+    fseek(infile, 0L, SEEK_END);
+    *length = ftell(infile);
+    fseek(infile, 0L, SEEK_SET);
+    *buffer = (char*)calloc(*length, sizeof(char));
+
+    /* memory error */
+    if(*buffer == NULL) {
+            printf("Could not allocate %ld bytes of memory!\n", *length);
+            return false;
+        }
+
+    length_read = fread(*buffer, sizeof(char), *length, infile);
+    *length = length_read;
+    fclose(infile);
+
+    return true;
+}
+
 int main(int argc, char **argv)
 {
-//    void *buffer;
-//    long length;
-//    struct pcap_pkthdr* pcap_hdr;
-//    int i;
-//    int loops = 1;
+    void *buffer;
+    long length;
+    struct pcap_pkthdr* pcap_hdr;
+    int i;
+    int loops = 1;
 
     TbIndication echoIndication(IfcNames_TbIndicationH2S);
     MemServerIndication memServerIndication(IfcNames_MemServerIndicationH2S);
@@ -237,29 +238,28 @@ int main(int argc, char **argv)
     device->writePacketBuff(0, 0xFACEBABE);
     device->readPacketBuff(0);
 
-    while(1) sleep(1);
 
-//    fprintf(stderr, "Attempts to read pcap file %s\n", argv[1]);
-//    if (!read_pcap_file(argv[1], &buffer, &length)) {
-//        perror("Failed to read file!");
-//        exit(-1);
-//    }
-//
-//    for (i = 0; i < loops; i++) {
-//        void* offset = static_cast<char *>(buffer) + sizeof(struct pcap_file_header);
-//
-//        while(offset < static_cast<char *>(buffer) + length) {
-//            pcap_hdr = (struct pcap_pkthdr*) offset;
-//            offset = static_cast<char *>(offset) + sizeof(struct pcap_pkthdr);
-//
-//            if ((quick_tx_send_packet((const void*)offset, pcap_hdr->caplen)) < 0) {
-//                printf("An error occurred while trying to send a packet\n");
-//                exit(-1);
-//            }
-//
-//            offset = static_cast<char *>(offset) + pcap_hdr->caplen;
-//        }
-//    }
+    fprintf(stderr, "Attempts to read pcap file %s\n", argv[1]);
+    if (!read_pcap_file(argv[1], &buffer, &length)) {
+        perror("Failed to read file!");
+        exit(-1);
+    }
+
+    for (i = 0; i < loops; i++) {
+        void* offset = static_cast<char *>(buffer) + sizeof(struct pcap_file_header);
+
+        while(offset < static_cast<char *>(buffer) + length) {
+            pcap_hdr = (struct pcap_pkthdr*) offset;
+            offset = static_cast<char *>(offset) + sizeof(struct pcap_pkthdr);
+
+            if ((quick_tx_send_packet((const void*)offset, pcap_hdr->caplen)) < 0) {
+                printf("An error occurred while trying to send a packet\n");
+                exit(-1);
+            }
+
+            offset = static_cast<char *>(offset) + pcap_hdr->caplen;
+        }
+    }
 
     return 0;
 }
