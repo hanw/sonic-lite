b089e56f4289a857fe94664d7c6f0bec37ad6d23
Han Wang <hwang@cs.cornell.edu>
2015-11-30 21:29:38 -0500

    dtp: updated blocksync test

---
+++ hw/tests/blocksync/Test.bsv
@@ -0,0 +1,79 @@
+package Test;
+
+import FIFO::*;
+import FIFOF::*;
+import DefaultValue::*;
+import SpecialFIFOs::*;
+import Vector::*;
+import BuildVector::*;
+import GetPut::*;
+import ClientServer::*;
+import Connectable::*;
+import HostInterface::*;
+
+import Pipe::*;
+import MemTypes::*;
+import Ethernet::*;
+import PacketBuffer::*;
+import BlockSync::*;
+
+interface TestIndication;
+   method Action done(Bit#(32) matchCount);
+endinterface
+
+interface TestRequest;
+   method Action writePacketData(Vector#(2, Bit#(64)) data, Bit#(1) sop, Bit#(1) eop);
+endinterface
+
+interface Test;
+   interface TestRequest request;
+endinterface
+
+module mkTest#(TestIndication indication) (Test);
+
+   let verbose = True;
+
+   Reg#(Bit#(32)) cycle <- mkReg(0);
+   FIFOF#(Bit#(66)) write_data <- mkFIFOF;
+
+   PacketBuffer buff <- mkPacketBuffer();
+   BlockSync bsync <- mkBlockSync;
+   mkConnection(toPipeOut(write_data), bsync.blockSyncIn);
+
+   rule every1;
+      cycle <= cycle + 1;
+      bsync.rx_ready(True);
+   endrule
+
+   rule readDataStart;
+      let pktLen <- buff.readServer.readLen.get;
+      if (verbose) $display(fshow(" read packt ") + fshow(pktLen));
+      buff.readServer.readReq.put(EtherReq{len: truncate(pktLen)});
+   endrule
+
+   rule readDataInProgress;
+      let v <- buff.readServer.readData.get;
+      if(verbose) $display("%d: mkTest.write_data v=%h", cycle, v);
+      write_data.enq(v.data[65:0]);
+      if (v.eop) begin
+         indication.done(0);
+      end
+   endrule
+
+   rule out;
+      let v = bsync.dataOut.first();
+      bsync.dataOut.deq;
+      if(verbose) $display("%d: blocksync in v=%h", cycle, v);
+   endrule
+
+   interface TestRequest request;
+      method Action writePacketData(Vector#(2, Bit#(64)) data, Bit#(1) sop, Bit#(1) eop);
+         EtherData beat = defaultValue;
+         beat.data = pack(reverse(data));
+         beat.sop = unpack(sop);
+         beat.eop = unpack(eop);
+         buff.writeServer.writeData.put(beat);
+      endmethod
+   endinterface
+endmodule
+endpackage: Test
---
+++ hw/tests/blocksync/test.cpp
@@ -0,0 +1,86 @@
+/* Copyright (c) 2015 Cornell University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <sys/mman.h>
+
+#include "MemServerRequest.h"
+#include "TestRequest.h"
+#include "TestIndication.h"
+#include "GeneratedTypes.h"
+#include "utils.h"
+#include "sonic_pcap_utils.h"
+
+#define DATA_WIDTH 128
+#define ITERATION 1024
+
+using namespace std;
+
+sem_t test_sem;
+static TestRequestProxy *device=0;
+
+void mem_copy(const void *buff, int length);
+
+class TestIndication : public TestIndicationWrapper {
+public:
+  virtual void done(uint32_t v){
+      sem_post(&test_sem);
+  }
+  TestIndication(int id) : TestIndicationWrapper(id){}
+};
+
+void mem_copy(const void *buff, int packet_size) {
+}
+
+int main(int argc, char **argv) {
+    TestIndication deviceIndication(IfcNames_TestIndicationH2S);
+    device = new TestRequestProxy(IfcNames_TestRequestS2H);
+
+    const std::string path="../../data/xgmii.data.scrambled";
+    std::ifstream traceinfo(path.c_str());
+    std::string line;
+    uint64_t data[2];
+    int sop, eop;
+
+    for (int i = 0; i < ITERATION; i=i+2) {
+        std::getline(traceinfo, line);
+        std::istringstream sstream(line);
+        std::string first_64;
+        std::string second_64;
+        sstream >> first_64;
+        sstream >> second_64;
+        data[1] = strtoul(first_64.c_str(), NULL, 16);
+        data[0] = strtoul(second_64.c_str(), NULL, 16);
+        sop = (i == 0) ? 1 : 0;
+        eop = (i == ITERATION-2) ? 1 : 0;
+        device->writePacketData(data, sop, eop);
+        PRINT_INFO("%016lx %016lx %d %d\n", data[1], data[0], sop, eop);
+    }
+
+    sem_wait(&test_sem);
+    return 0;
+}
