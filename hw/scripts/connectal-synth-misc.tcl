20812a3b9eeffd1aaea7c0fe15facf2948fbc298
Ki Suh Lee <kslee@cs.cornell.edu>
2016-01-11 11:33:13 -0500

    reverting paths for ip scripts

--- hw/scripts/connectal-synth-misc.tcl
+++ hw/scripts/connectal-synth-misc.tcl
@@ -2,5 +2,5 @@ source "board.tcl"
 source "$connectaldir/scripts/connectal-synth-ip.tcl"
 
 #if {[info exists USE_ALTERA_CLKCTRL]} {
-   fpgamake_altera_ipcore_qsys /home/kslee/sonic/sonic-lite/hw/qsys/altera_clkctrl.qsys 14.0 altera_clkctrl
+   fpgamake_altera_ipcore_qsys ../../hw/qsys/altera_clkctrl.qsys 14.0 altera_clkctrl
 #}
--- hw/scripts/connectal-synth-pma.tcl
+++ hw/scripts/connectal-synth-pma.tcl
@@ -2,5 +2,5 @@ source "board.tcl"
 source "$connectaldir/scripts/connectal-synth-ip.tcl"
 
 #if {[info exists USE_ALTERA_SV_10G_PMA]} {
-   fpgamake_altera_ipcore_qsys /home/kslee/sonic/sonic-lite/hw/qsys/sv_10g_pma.qsys 14.0 sv_10g_pma
+   fpgamake_altera_ipcore_qsys ../../hw/qsys/sv_10g_pma.qsys 14.0 sv_10g_pma
 #}
